3f78719179dcd75b0e6746ed7eff7ea5ca9b103f
wxrbwran <wxrbw@qq.com>
2021-08-23 23:31:40 +0800

    add watcher

---
+++ log-collector/src/main/java/com/xiaoram/log/collector/controller/WatcherController.java
@@ -0,0 +1,19 @@
+package com.xiaoram.log.collector.controller;
+
+import com.xiaoram.log.collector.entity.AccurateWatcherMessage;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+
+@RestController
+public class WatcherController {
+	
+	@RequestMapping(value ="/accurateWatch")
+	public String watch(@RequestBody AccurateWatcherMessage accurateWatcherMessage) {
+		String ret = JSON.toJSONString(accurateWatcherMessage);
+		System.err.println("----告警内容----:" + ret);
+		return "is watched" + ret;
+	}
+}
