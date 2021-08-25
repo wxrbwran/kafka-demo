dfb52837386a3809092431fc9e44f22562e182c2
wuxiaoran <wuxiaoran@xinzhili.cn>
2021-08-25 17:43:54 +0800

    修复模版错误

--- elk-conf/xpack/watcher/error_log_template.js
+++ elk-conf/xpack/watcher/error_log_template.js
@@ -1,7 +1,7 @@
-// PUT _template/error-log-
 // 创建watcher前必须配置模版，不然匹配不到结果
+PUT _template/error-log-collector-
 {
-    "template": "error-log-*",
+    "template": "error-log-collector*",
     "order": 0,
     "settings": {
         "index": {
